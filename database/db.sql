ebd0053249e731be626283a5abc1d0b9a16ca0a3
Ann Keosykhao <72222180+keosykhao@users.noreply.github.com>
2021-06-28 18:28:31 -0400

    Update db.sql
    creating our table

--- database/db.sql
+++ database/db.sql
@@ -1,3 +1,11 @@
 -- testing
 -- this is so frustrating
 -- testinhg this database
+CREATE TABLE  restaurants(
+    id INT,
+    name VARCHAR(50),
+    location VARCHAR(50),
+    price_range INT
+);
+
+INSERT INTO restaurants (id, name, location, price_range)  values(123,'mcdonalds', 'New York', 3 )
